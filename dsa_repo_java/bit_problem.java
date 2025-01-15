ef4ce99fabeb4ff38cacc0352fb9daeaf132fe64
CoderCraftsman9569 <my7186695@gmail.com>
2025-01-15 21:24:49 +0530

    change

---
+++ dsa_repo_java/bit_problem.java
@@ -0,0 +1,38 @@
+package dsa_repo_java;
+
+public class bit_problem {
+    public int countSetBits(int n) {
+        int count = 0;
+        while (n != 0) {
+            count += n & 1;
+            n >>= 1;
+        }
+        return count;
+    }
+    
+    public int minimizeXor(int num1, int num2) {
+        int bits = countSetBits(num2);
+        int result = 0;
+        for (int i = 31; i >= 0 && bits > 0; i--) {
+            if ((num1 & (1 << i)) != 0) {
+                result |= (1 << i);
+                bits--;
+            }
+        }
+        for (int i = 0; i < 32 && bits > 0; i++) {
+            if ((result & (1 << i)) == 0) {
+                result |= (1 << i);
+                bits--;
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        bit_problem bp = new bit_problem();
+        int num1 = 10;
+        int num2 = 20;
+        System.out.println(bp.minimizeXor(num1, num2));
+        
+    }
+    
+}
